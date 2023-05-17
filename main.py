83799c0bf700bdb48275dd6c89a4fcb7fdf0501a
Szymon Nowicki <s.nowick@wp.pl>
2023-05-17 09:05:15 +0200

    BOI

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+
+@app.post("/items/")
+async def create_item(item: StudentCreateSchema):
+    return item
