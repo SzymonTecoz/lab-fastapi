2d0f927a12ce3302871f4d0ad7212629d384ee4f
Szymon Nowicki <s.nowick@wp.pl>
2023-05-24 09:13:05 +0200

    Commituje chaos

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 lab-fastapi z zajęć 17.05.2023
 Sam nie wiem co się dzieje
 Polecam pythonga
+test
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
+from functools import lru_cache
 
 class StudentCreateSchema(BaseModel):
     first_name: str
     last_name: str
+    class Config(BaseModel):
+        schema_extra = {
+            "Student 1":{
+                "first_name": "Andrzej",
+                "last_name": "Kowalski"
+            }
+        }
 app = FastAPI()
 
+#class StudentUpdateSchema(BaseModel):
+students =[]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
+@app.get("/students/{id}")
+async def root(id:int):
+    if len(students)<= id:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"student": students[id]}
+
+async def read_item(id: int):
+    return {"item_id": id}
 
 
-
-@app.post("/items/")
+@app.post("/students")
 async def create_item(item: StudentCreateSchema):
+    students.append(item)
     return item
